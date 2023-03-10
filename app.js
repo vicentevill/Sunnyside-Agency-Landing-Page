4c57a00ade8a61b04784caa826344c9067de0ede
thekingvice <villarrealvicente92@gmail.com>
2023-03-09 16:06:56 -0800

    commit

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const hamburgerIcon = document.querySelector(".header__hamburger-icon");
+const hamburgerNav = document.querySelector(".header__nav-mobile");
+let hamburgerStateCounter = 0;
 
+hamburgerIcon.addEventListener("click", () => {
+  if (hamburgerStateCounter === 0) {
+    hamburgerStateCounter = 1;
+    hamburgerIcon.style.opacity = "75%";
+    hamburgerNav.style.display = "flex";
+  } else {
+    hamburgerStateCounter = 0;
+    hamburgerIcon.style.opacity = "";
+    hamburgerNav.style.display = "";
+  }
+});
